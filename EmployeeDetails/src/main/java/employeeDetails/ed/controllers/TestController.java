c014bdb1a16e8204013c759bdcab32fa0c9358a1
shraddak <shraddak@ULTP852.synerzipune.local>
2020-12-29 19:46:17 +0530

    adding form controls and calling web service from angular

--- EmployeeDetails/src/main/java/employeeDetails/ed/controllers/TestController.java
+++ EmployeeDetails/src/main/java/employeeDetails/ed/controllers/TestController.java
@@ -7,6 +7,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,10 +15,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+
 import employeeDetails.ed.model.Employee;
 import employeeDetails.ed.service.EmployeeService;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class TestController {
 @Autowired
 @Qualifier("employeeService")
