ac6fb367b29ce66ebf1083026ae35d3adc52d3ee
Shradda Kachhi <shraddak@synerzipune.local>
2021-01-21 18:50:31 +0530

    adding udemy training practise

---
+++ training/src/app/firstComponent/first.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component ({
+    selector :'app-first',//we can have a class like '.app-firs' and as an element [app-first]
+    templateUrl :'./first.component.html',
+    styleUrls :[]//its an array because we can have more than one file
+})
+export class FirstComponent{
+
+}
