6a98625c24181b3b728330e23fca6d9e59a5bb79
Shradda Kachhi <shraddak@synerzipune.local>
2021-01-22 20:05:16 +0530

    adding dirsctives

--- training/src/app/app.module.ts
+++ training/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { FirstComponent } from './firstComponent/first.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { FirstComponent } from './firstComponent/first.component';
     FirstComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- training/src/app/firstComponent/first.component.ts
+++ training/src/app/firstComponent/first.component.ts
@@ -3,8 +3,24 @@ import { Component } from "@angular/core";
 @Component ({
     selector :'app-first',//we can have a class like '.app-firs' and as an element [app-first]
     templateUrl :'./first.component.html',
-    styleUrls :[]//its an array because we can have more than one file
+    styles :['.ngclasstest{color:blue;background-color:pink};']//its an array because we can have more than one file
 })
 export class FirstComponent{
+changeDisabled=false;
+inputEvent='';
+assgTextBox ='';
+    constructor(){
+        //this is an angular fuction takes two parameter
+        //first is the function 2nd the timout time when you have to run that function
+    setTimeout(()=>this.changeDisabled=true,2000);
+}
 
+onEachKeyInput(event:Event){
+    console.log(event);
+this.inputEvent=(<HTMLInputElement> event.target).value;
+console.log(this.inputEvent);
+}
+getColor(){
+    return 'turquoise';
+}
 }
